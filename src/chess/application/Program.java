aa6307bcc1b1f00331cd3bc970ad833f4eaa2be7
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2024-12-26 20:10:41 -0300

    First class: Position

--- src/chess/application/Program.java
+++ src/chess/application/Program.java
@@ -1,9 +1,12 @@
 package chess.application;
 
+import chess.boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
     
 }
