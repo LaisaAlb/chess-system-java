b17c63d811dd3d67a6c5a6cf002e691c8c41c8c7
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2025-02-10 13:47:00 -0300

    Swintching player each turn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,13 +8,33 @@ import boardgame.Position;
 
 public class ChessMatch {
 
+    private int turn;
+    private Color currentPlayer; 
     private Board board;
 
     public ChessMatch() {
         board = new Board(8, 8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initialSetup();
     }
 
+    public int getTurn() {
+        return turn;
+    }
+    
+    public Color getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+    
     public ChessPiece[][] getPieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
         for (int i = 0; i < board.getRows(); i++) {
@@ -41,6 +61,7 @@ public class ChessMatch {
         validateSourcePosition (source);
         validateTargetPosition(source, target);
         Piece capturePiece = makeMove(source, target);
+        nextTurn();
         return (ChessPiece)capturePiece;
     }    
 
@@ -63,6 +84,9 @@ public class ChessMatch {
         if(!board.thereIsAPiece(position)){
             throw new ChessException("There is no piece on source position");
         }
+        if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()){
+            throw new ChessException("The chosen piece is not yours"); 
+        }
         if(!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("There is no possible moves for the chosen piece"); 
         }
@@ -79,5 +103,10 @@ public class ChessMatch {
         if(!board.piece(source).possibleMove(target)){
             throw new ChessException("The chosen piece can't move to target position");
         }
+    }
+    
+    private void nextTurn(){
+        turn ++; 
+        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE; 
     }
 }
