68f7501e5585579f54fa375d3b8b227fb42be32f
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2025-02-12 08:49:50 -0300

    Project completed

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
 
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        List<ChessPiece> captured = new ArrayList<>(); 
+        List<ChessPiece> captured = new ArrayList<>();
 
         while (!chessMatch.getCheckMate()) {
             try {
@@ -25,19 +25,28 @@ public class Program {
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
 
-                boolean[][] possibleMoves = chessMatch.possibleMoves(source); 
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
-                
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
-                ChessPiece capturedPiece = chessMatch.perfomChessMove(source, target);
-                if(capturedPiece != null){
-                    captured.add(capturedPiece); 
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+                if (capturedPiece != null) {
+                    captured.add(capturedPiece);
                 }
-                
+
+                if (chessMatch.getPromoted() != null) {
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+                        System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
+                    chessMatch.replacePromotedPiece(type);
+                }
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
@@ -46,7 +55,6 @@ public class Program {
                 sc.nextLine();
             }
         }
-        
         UI.clearScreen();
         UI.printMatch(chessMatch, captured);
     }
